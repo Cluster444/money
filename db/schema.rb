f68119d6fba853d024c7ce5a442a494c807c22b1
Chris Covington <chris.covington@hey.com>
2025-10-21 17:05:31 -0700

    migration for STI table kind

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_17_211024) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_21_220000) do
   create_table "accounts", force: :cascade do |t|
     t.string "kind", null: false
     t.string "name", null: false
