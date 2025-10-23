a9770d1cc89bd372c64e5aba4b001188b9be8164
Chris Covington <chris.covington@hey.com>
2025-10-23 12:16:35 -0700

    upgrade rails to 8.1 and ruby to 3.4.7

--- db/schema.rb
+++ db/schema.rb
@@ -10,74 +10,74 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_21_220000) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_22_212019) do
   create_table "accounts", force: :cascade do |t|
-    t.string "kind", null: false
-    t.string "name", null: false
     t.boolean "active", default: true, null: false
-    t.bigint "debits", default: 0, null: false
-    t.bigint "credits", default: 0, null: false
-    t.json "metadata", default: {}, null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "credits", default: 0, null: false
+    t.bigint "debits", default: 0, null: false
+    t.string "kind", null: false
+    t.json "metadata", default: {}, null: false
+    t.string "name", null: false
     t.integer "organization_id"
+    t.datetime "updated_at", null: false
     t.index ["organization_id"], name: "index_accounts_on_organization_id"
   end
 
   create_table "adjustments", force: :cascade do |t|
     t.integer "account_id", null: false
+    t.datetime "created_at", null: false
     t.bigint "credit_amount"
     t.bigint "debit_amount"
     t.text "note"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_adjustments_on_account_id"
   end
 
   create_table "organizations", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_organizations_on_user_id"
   end
 
   create_table "schedules", force: :cascade do |t|
-    t.string "name", null: false
     t.bigint "amount"
-    t.string "period"
-    t.integer "frequency"
-    t.date "starts_on", null: false
-    t.date "ends_on"
-    t.date "last_materialized_on"
+    t.datetime "created_at", null: false
     t.integer "credit_account_id", null: false
     t.integer "debit_account_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.date "ends_on"
+    t.integer "frequency"
+    t.date "last_materialized_on"
+    t.string "name", null: false
+    t.string "period"
     t.integer "relative_account_id"
+    t.date "starts_on", null: false
+    t.datetime "updated_at", null: false
     t.index ["credit_account_id"], name: "index_schedules_on_credit_account_id"
     t.index ["debit_account_id"], name: "index_schedules_on_debit_account_id"
     t.index ["relative_account_id"], name: "index_schedules_on_relative_account_id"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "transfers", force: :cascade do |t|
-    t.string "state", default: "pending", null: false
     t.bigint "amount", null: false
+    t.datetime "created_at", null: false
+    t.integer "credit_account_id", null: false
+    t.integer "debit_account_id", null: false
     t.date "pending_on", null: false
     t.date "posted_on"
-    t.integer "debit_account_id", null: false
-    t.integer "credit_account_id", null: false
     t.integer "schedule_id"
-    t.datetime "created_at", null: false
+    t.string "state", default: "pending", null: false
     t.datetime "updated_at", null: false
     t.index ["credit_account_id"], name: "index_transfers_on_credit_account_id"
     t.index ["debit_account_id"], name: "index_transfers_on_debit_account_id"
@@ -85,11 +85,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_21_220000) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "email_address", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
